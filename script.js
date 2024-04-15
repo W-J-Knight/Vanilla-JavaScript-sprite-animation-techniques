7f9de9d8323e6ed34556b8de566cdc750e7802f3
William Knight <William.Jeffery.Knight@protonmail.com>
2024-04-14 21:20:05 -0400

    finished Project 1: Vanilla JavaScript sprite animation techniques plan to come back to make it a class

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let playerState = 'roll';
+const dropdown = document.getElementById('animations')
+dropdown.addEventListener('change', function(e){
+  playerState = e.target.value;
+})
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 // console.log(ctx);
@@ -9,27 +14,76 @@ const playerImage = new Image();
 playerImage.src = "shadow_dog.png";
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
+
 let gameframe = 0;
 const staggerFrame = 5;
+const spriteAnimations = []
+const animationStates = [
+  {
+    name: 'idle',
+    frames: 7,
+  },
+  {
+    name: 'jump',
+    frames: 7,
+  },
+  {
+    name: 'fall',
+    frames: 7,
+  },
+  {
+    name: 'run',
+    frames: 9,
+  },
+  {
+    name: 'dizzy',
+    frames: 11,
+  },
+  {
+    name: 'sit',
+    frames: 5,
+  },
+  {
+    name: 'roll',
+    frames: 7,
+  },
+  {
+    name: 'bite',
+    frames: 7,
+  },
+  {
+    name: 'ko',
+    frames: 12,
+  },
+  {
+    name: 'getHit',
+    frames: 4,
+  }
+]
 
+animationStates.forEach((state,index) => {
+  let frames = {
+    loc:[]
+  }
+  for(let j = 0; j < state.frames;  j++){
+    let positionX = j* spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({x: positionX, y: positionY})
+  }
+  spriteAnimations[state.name] = frames
+})
+console.log(spriteAnimations)
 function animate() {
   // clearRect(x, y, width, height)
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  // fillRect(x, y, width, height)
-  //   ctx.fillRect(50, 50, 100, 100);
-  //drawImage(image, dx, dy)
-  //   ctx.drawImage(playerImage, 50, 50);
-  //   drawImage(image, dx, dy, dWidth, dHeight) array of images
-  // ctx.drawImage(playerImage, 50, 50, CANVAS_WIDTH, CANVAS_HEIGHT);
   // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight): sprite sheets
-  let position = Math.floor(gameframe/staggerFrame) % 6;
-  frameX = spriteWidth * position
+  let position = Math.floor(gameframe/staggerFrame) % spriteAnimations[playerState].loc.length;
+  let frameX = spriteWidth * position;
+  let frameY = spriteAnimations[playerState].loc[position].y
   ctx.drawImage(
     playerImage,
     frameX,
-    frameY * spriteHeight,
+    frameY,
     spriteWidth,
     spriteHeight,
     0,
